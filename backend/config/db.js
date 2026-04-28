e7d35898b9067c875c67a3f4867e06dcc4f36264
Aakash Haygonde <177605571+aakashaygonde@users.noreply.github.com.>
2026-04-29 01:25:46 +0530

    Fixed mongo URI

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,9 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect("process.env.MONGO_URI", {
-            // Options are parsed automatically in newer versions of Mongoose
-        });
+        const conn = await mongoose.connect(process.env.MONGO_URI);
         console.log(`MongoDB Connected: ${conn.connection.host}`);
     } catch (error) {
         console.error(`Error connecting to MongoDB: ${error.message}`);
