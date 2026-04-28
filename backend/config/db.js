cf909f4e6951da6c58693afa2feba0564a39f926
Aakash <177605571+aakashaygonde@users.noreply.github.com>
2026-04-28 12:53:28 +0530

    Update db.js

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ const mongoose = require('mongoose');
 
 const connectDB = async () => {
     try {
-        const conn = await mongoose.connect(process.env.MONGO_URI, {
+        const conn = await mongoose.connect("mongodb+srv://deshmukharya2803_db_user:<WvnhOnp4AWdBrCLC>@cluster0.azvo3o8.mongodb.net/?appName=Cluster0", {
             // Options are parsed automatically in newer versions of Mongoose
         });
         console.log(`MongoDB Connected: ${conn.connection.host}`);
